b668a229dbadc3b92784f481aeb325aecd84a498
AshleyCol <ashe.coleman1022@gmail.com>
2026-09-22 21:02:34 -0400

    Calculation of totals and averages complete and functional

--- Marathon Runners/Marathon Runners.cpp
+++ Marathon Runners/Marathon Runners.cpp
@@ -8,21 +8,27 @@ const int runnerCount = 5;
 const string dataFile = "runners.txt";
 
 void readRunnerData(string filename, string names[runnerCount], int dailyMiles[runnerCount][7]);
+void calculateTotalsAndAverages(int dailyMiles[runnerCount][7], int averages[runnerCount], int totals[runnerCount]);
 
 
 int main()
 {
     string runnerNames[runnerCount];
     int milesRun[runnerCount][7];
+    int averageMiles[runnerCount];
+    int totalMiles[runnerCount];
 
     readRunnerData(dataFile, runnerNames, milesRun);
-
+    calculateTotalsAndAverages(milesRun, averageMiles, totalMiles);
+    
 }
+
+
 
 void readRunnerData(string filename, string names[runnerCount], int dailyMiles[runnerCount][7]) {
     ifstream inFile(filename);
-    
-    
+
+
     if (!inFile) {
         cout << "Error occured while opening file for reading!" << endl;
 
@@ -39,5 +45,23 @@ void readRunnerData(string filename, string names[runnerCount], int dailyMiles[r
 
             ++dayCounter;
         }
+    }
+}
+
+
+
+void calculateTotalsAndAverages(int dailyMiles[runnerCount][7], int averages[runnerCount], int totals[runnerCount]) {
+    for (int i = 0; i < runnerCount; i++) {
+        int dayCounter = 0;
+        int total = 0;
+
+        while (dayCounter < 7) {
+            total = total + dailyMiles[i][dayCounter];
+
+            ++dayCounter;
+        }
+        
+        totals[i] = total;
+        averages[i] = total / 7;
     }
 }
