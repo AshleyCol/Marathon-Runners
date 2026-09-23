21d93c579c0906e977033de751240c9c8cdee0f7
AshleyCol <ashe.coleman1022@gmail.com>
2026-09-22 22:04:24 -0400

    displayResults funcitoning properly and displaying the proper formatted table of data

--- Marathon Runners/Marathon Runners.cpp
+++ Marathon Runners/Marathon Runners.cpp
@@ -7,25 +7,26 @@ using namespace std;
 const int runnerCount = 5;
 const string dataFile = "runners.txt";
 
-void readRunnerData(string filename, string names[runnerCount], int dailyMiles[runnerCount][7]);
-void calculateTotalsAndAverages(int dailyMiles[runnerCount][7], int averages[runnerCount], int totals[runnerCount]);
+void readRunnerData(string filename, string names[runnerCount], double dailyMiles[runnerCount][7]);
+void calculateTotalsAndAverages(double dailyMiles[runnerCount][7], double averages[runnerCount], double totals[runnerCount]);
+void displayResults(string names[runnerCount], double dailyMiles[runnerCount][7], double averages[runnerCount], double totals[runnerCount]);
 
 
 int main()
 {
     string runnerNames[runnerCount];
-    int milesRun[runnerCount][7];
-    int averageMiles[runnerCount];
-    int totalMiles[runnerCount];
+    double milesRun[runnerCount][7];
+    double averageMiles[runnerCount];
+    double totalMiles[runnerCount];
 
     readRunnerData(dataFile, runnerNames, milesRun);
     calculateTotalsAndAverages(milesRun, averageMiles, totalMiles);
-    
+    displayResults(runnerNames, milesRun, averageMiles, totalMiles);
 }
 
 
 
-void readRunnerData(string filename, string names[runnerCount], int dailyMiles[runnerCount][7]) {
+void readRunnerData(string filename, string names[runnerCount], double dailyMiles[runnerCount][7]) {
     ifstream inFile(filename);
 
 
@@ -50,10 +51,10 @@ void readRunnerData(string filename, string names[runnerCount], int dailyMiles[r
 
 
 
-void calculateTotalsAndAverages(int dailyMiles[runnerCount][7], int averages[runnerCount], int totals[runnerCount]) {
+void calculateTotalsAndAverages(double dailyMiles[runnerCount][7], double averages[runnerCount], double totals[runnerCount]) {
     for (int i = 0; i < runnerCount; i++) {
         int dayCounter = 0;
-        int total = 0;
+        double total = 0;
 
         while (dayCounter < 7) {
             total = total + dailyMiles[i][dayCounter];
@@ -64,4 +65,22 @@ void calculateTotalsAndAverages(int dailyMiles[runnerCount][7], int averages[run
         totals[i] = total;
         averages[i] = total / 7;
     }
+}
+
+void displayResults(string names[runnerCount], double dailyMiles[runnerCount][7], double averages[runnerCount], double totals[runnerCount]) {
+
+    cout << setw(10) <<"Names" << setw(10) << "Sunday" << setw(10) << "Monday" << setw(10) << "Tuesday" << setw(10) << "Wednesday" << setw(10) << "Thursday" << setw(10) << "Friday" << setw(10) << "Saturday" << setw(14) << "Total Miles" << setw(20) << "Average Miles/Day" << endl;
+
+    for (int i = 0; i < runnerCount; i++) {
+        
+        cout << setw(10) << names[i];
+
+        for (int day = 0; day < 7; day++) {
+            cout << setw(10) << dailyMiles[i][day];
+        }
+
+        cout << setw(14) << totals[i] << setw(20) << averages[i] << endl;
+    }
+
+
 }
